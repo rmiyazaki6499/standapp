ce91f0f208417e3bee35df3e6250de60afcd35f9
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-04-21 21:17:24 -0700

    Fixed the labeling for Django Admin of our models

--- backend/standapp_be/app/models.py
+++ backend/standapp_be/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 # Create your models here.
@@ -14,6 +15,8 @@ class Progress(models.Model):
 
 class Standup(models.Model):
     date = models.DateTimeField('date of standup')
+    def __str__(self):
+        return self.date.strftime("%Y-%d-%b")
 
 
 
