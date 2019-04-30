23ec498ee9663cc502d24154310fb89df4f65797
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-04-30 15:43:08 -0700

    Added images to About Us Page

--- frontend/src/app/components/about/about.component.ts
+++ frontend/src/app/components/about/about.component.ts
@@ -3,9 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   template: `
+  <img src="assets/img/standapp_logo.png">
   <h1>About Us</h1>
+
     <p>
     Draychee is a Partnership between Andreas Lorentson and Ryuichi Miyazaki. They both met March 19th, 2019 when they started at FastSpring together the same day.
+    </p>
+    <img src="assets/img/rmiyazaki.jpg">
+    <img src="assets/img/aloretson.jpg">
 
   `,
   styleUrls: []
