18d6f78d8d3a61b3302338a768004e692c4a7f3c
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-05-01 21:01:41 -0700

    Forgot app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,16 +5,6 @@ import { ApiService } from './api.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   providers: [ApiService],
-  template: `
-  <!-- header -->
-  <app-header></app-header>
-
-  <!-- routes will be rendered here -->
-  <router-outlet></router-outlet>
-
-  <!-- footer -->
-  <app-footer></app-footer>
-`,
 styles: []
 })
 
