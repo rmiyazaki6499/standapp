0373a4023e909273cb166970d1412d6fed6fe791
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-05-18 20:09:07 -0700

    Added Permissions, Authentication for Login, Logout and Registration. Moved the login, signup buttons to right side of the navbar

--- backend/standapp_be/app/views.py
+++ backend/standapp_be/app/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, generics
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from standapp_be.app.serializers import *
 from .models import Progress, Standup
@@ -8,17 +10,27 @@ from .models import Progress, Standup
 class ProgressViewSet(viewsets.ModelViewSet):
     queryset = Progress.objects.all()
     serializer_class = ProgressSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
 
     def list(self, request, *args, **kwargs):
         progresses = Progress.objects.all()
         serializer = ProgressSerializer(progresses, many=True)
         return Response(serializer.data)
 
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
+
 
 class StandupDetailViewSet(viewsets.ModelViewSet):
     queryset = Progress.objects.all()
     serializer_class = ProgressSerializer
     filterset_fields = ('standupId')
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
 
     def list(self, request, *args, **kwargs):
         standup_id = request.query_params.get('standupId', None)
@@ -26,12 +38,33 @@ class StandupDetailViewSet(viewsets.ModelViewSet):
         serializer = ProgressSerializer(progresses, many=True)
         return Response(serializer.data)
 
+    def get(self, request, format=None):
+        content = {
+            'user': unicode(request.user),  # `django.contrib.auth.User` instance.
+            'auth': unicode(request.auth),  # None
+        }
+        return Response(content)
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
+
 
 class StandupViewSet(viewsets.ModelViewSet):
     queryset = Standup.objects.all()
     serializer_class = StandupSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (TokenAuthentication,)
 
     def list(self, request, *args, **kwargs):
         standup = Standup.objects.all()
         serializer = StandupSerializer(standup, many=True)
         return Response(serializer.data)
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
