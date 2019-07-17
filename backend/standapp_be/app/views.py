735bc43ad0d7fe7505f0795c3f1caca99d1bcf2e
alorents <andreasphoenix@gmail.com>
2019-07-16 20:32:23 -0700

    Fix for progresses bug
    -StandupDetailView was somehow added back in removed it again
    -Made ProgressViewSet in charge of filtering progresses by standup

--- backend/standapp_be/app/views.py
+++ backend/standapp_be/app/views.py
@@ -1,5 +1,4 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets, generics
+from rest_framework import viewsets
 
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
@@ -8,44 +7,20 @@ from rest_framework.permissions import IsAuthenticated
 from standapp_be.app.serializers import *
 from .models import Progress, Standup
 
+
 class ProgressViewSet(viewsets.ModelViewSet):
-    queryset = Progress.objects.all()
-    serializer_class = ProgressSerializer
-    permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication,)
-
-    def list(self, request, *args, **kwargs):
-        progresses = Progress.objects.all()
-        serializer = ProgressSerializer(progresses, many=True)
-        return Response(serializer.data)
-
-    def get(self, request, format=None):
-        content = {
-            'status': 'request was permitted'
-        }
-        return Response(content)
-
-
-class StandupDetailViewSet(viewsets.ModelViewSet):
     queryset = Progress.objects.all()
     serializer_class = ProgressSerializer
     filterset_fields = ('standupId')
     permission_classes = (IsAuthenticated,)
     authentication_classes = (TokenAuthentication,)
 
-
     def list(self, request, *args, **kwargs):
         standup_id = request.query_params.get('standupId', None)
         progresses = Progress.objects.filter(standup=standup_id)
         serializer = ProgressSerializer(progresses, many=True)
+        print(standup_id)
         return Response(serializer.data)
-
-    def get(self, request, format=None):
-        content = {
-            'user': unicode(request.user),  # `django.contrib.auth.User` instance.
-            'auth': unicode(request.auth),  # None
-        }
-        return Response(content)
 
     def get(self, request, format=None):
         content = {
