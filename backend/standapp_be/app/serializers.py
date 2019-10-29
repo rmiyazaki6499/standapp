33b885aa2f9707d46474d74e1b95c15d4c7d69f0
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-10-28 20:26:25 -0700

    Created custom user model

--- backend/standapp_be/app/serializers.py
+++ backend/standapp_be/app/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from .models import Progress, Standup, User
 
@@ -17,4 +16,4 @@ class StandupSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name')
+        fields = ('id', 'username', 'email')
