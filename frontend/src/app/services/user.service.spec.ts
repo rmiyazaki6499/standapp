b0656c835d64b3173d3428c95a035c5e207c56fa
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2019-11-16 18:34:29 -0800

    Added unit test for getUsername

--- frontend/src/app/services/user.service.spec.ts
+++ frontend/src/app/services/user.service.spec.ts
@@ -1,17 +1,52 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule],
-    providers: [
-      UserService
-    ]
-  }));
+  let service: UserService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [UserService]
+  })
+  service = TestBed.get(UserService)
+});
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
     expect(service).toBeTruthy();
   });
+
+  describe('getUsername', () => {
+    let store = {};
+    const mockSessionStorage = {
+      getItem: (key: string): string => {
+        return key in store ? store[key] : null;
+      },
+      setItem: (key: string, value: string) => {
+        store[key] = `${value}`;
+      },
+      clear: () => {
+        store = {};
+      }
+    };
+    beforeEach(() => {
+      spyOn(sessionStorage, 'getItem')
+        .and.callFake(mockSessionStorage.getItem);
+      spyOn(sessionStorage, 'clear')
+        .and.callFake(mockSessionStorage.clear);
+    });
+    it('should retrieve username from sessionStorage', () => {
+      mockSessionStorage.setItem('username', 'username');
+      expect(service.getUsername()).toBeTruthy();
+    });
+
+    it('should not be able to retriece username from sessionStorage', () => {
+      mockSessionStorage.clear();
+      expect(service.getUsername()).toBeFalsy();
+    });
+  });
+
 });
