fb611a3079dc70d34fe2bc8156f552522aea8e79
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-04-26 01:14:26 -0700

    Changed database back to sqlite

--- backend/standapp_be/settings.py
+++ backend/standapp_be/settings.py
@@ -83,7 +83,7 @@ TEMPLATES = [
             ],
         },
     },
-] 
+]
 
 WSGI_APPLICATION = 'standapp_be.wsgi.application'
 
@@ -91,16 +91,16 @@ WSGI_APPLICATION = 'standapp_be.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default' = dj_database_url.config(conn_max_age=600, ssl_require=True)
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
+#     'default' = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 AUTH_USER_MODEL = 'user.User'
 
